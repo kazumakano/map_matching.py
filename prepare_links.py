95726367151e4eb87d9950d741f352c8af668762
kazumakano <kazumakano@outlook.jp>
2022-01-19 13:34:52 +0900

    update prepare_links.py

--- prepare_links.py
+++ prepare_links.py
@@ -2,12 +2,14 @@ import os.path as path
 from datetime import datetime
 from glob import glob
 import particle_filter.script.parameter as pf_param
+import particle_filter.script.utility as pf_util
+import script.parameter as param
 from particle_filter.script.log import Log
 from script.map import Map
 
 
-def prepare_links(enable_csv: bool = False, enable_pkl: bool = False) -> None:
-    map = Map(Log(datetime(2000, 1, 1), datetime(2000, 1, 1), glob(path.join(pf_param.ROOT_DIR, "log/observed/*.csv"))[0]).mac_list)    # whatever is fine
+def prepare_links(result_file_name: str, enable_csv: bool = False, enable_pkl: bool = False) -> None:
+    map = Map(Log(datetime(2000, 1, 1), datetime(2000, 1, 1), glob(path.join(pf_param.ROOT_DIR, "log/observed/*.csv"))[0]).mac_list, result_file_name)    # whatever is fine
 
     if enable_csv:
         map.export_links_to_csv()
@@ -19,7 +21,6 @@ def prepare_links(enable_csv: bool = False, enable_pkl: bool = False) -> None:
 
 if __name__ == "__main__":
     import argparse
-    import script.parameter as param
     from script.parameter import set_params
 
     parser = argparse.ArgumentParser()
@@ -31,8 +32,8 @@ if __name__ == "__main__":
     if (not args.csv) and (not args.pkl):
         raise Warning("prepare_links.py: set flags in order to export")
 
-    set_params(args.conf_file)
+    conf = set_params(args.conf_file)
     param.ENABLE_DRAW_LINKS = True
     param.SET_LINKS_POLICY = 1
 
-    prepare_links(args.csv, args.pkl)
+    prepare_links(pf_util.gen_file_name() if conf["result_file_name"] is None else str(conf["result_file_name"]), args.csv, args.pkl)
