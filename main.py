3407db1c0b5f31373ebc6252fb17fa4b30ecde3d
kazumakano <kazumakano@outlook.jp>
2022-01-29 20:55:12 +0900

    update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from script.particle import Particle
 
 
 def _set_main_params(conf: dict[str, Any]) -> None:
-    global BEGIN, END, LOG_FILE, INIT_DIRECT, INIT_DIRECT_SD, INIT_POS, INIT_POS_SD, LOST_TRAJECTORY_POLICY, PARTICLE_NUM, RESULT_DIR_NAME
+    global BEGIN, END, LOG_FILE, INIT_DIRECT, INIT_DIRECT_SD, INIT_POS, INIT_POS_SD, LOST_TJ_POLICY, PARTICLE_NUM, RESULT_DIR_NAME
 
     BEGIN = datetime.strptime(conf["begin"], "%Y-%m-%d %H:%M:%S")
     END = datetime.strptime(conf["end"], "%Y-%m-%d %H:%M:%S")
@@ -23,11 +23,11 @@ def _set_main_params(conf: dict[str, Any]) -> None:
     INIT_DIRECT_SD = np.float16(conf["init_direct_sd"])
     INIT_POS = np.array(conf["init_pos"], dtype=np.float16)
     INIT_POS_SD = np.float16(conf["init_pos_sd"])
-    LOST_TRAJECTORY_POLICY = np.int8(conf["lost_trajectory_policy"])
+    LOST_TJ_POLICY = np.int8(conf["lost_tj_policy"])
     PARTICLE_NUM = np.int16(conf["particle_num"])
     RESULT_DIR_NAME = None if conf["result_dir_name"] is None else str(conf["result_dir_name"])
 
-def map_matching(conf: dict[str, Any]) -> None:
+def map_matching(conf: dict[str, Any], enable_show: bool = True) -> None:
     log = Log(BEGIN, END, path.join(pf_param.ROOT_DIR, "log/observed/", LOG_FILE))
     result_dir = pf_util.make_result_dir(RESULT_DIR_NAME)
     map = Map(log.mac_list, result_dir)
@@ -47,47 +47,47 @@ def map_matching(conf: dict[str, Any]) -> None:
     for i in range(PARTICLE_NUM):
         poses[i] = np.random.normal(loc=INIT_POS, scale=INIT_POS_SD, size=2).astype(np.float16)
         directs[i] = np.float16(np.random.normal(loc=INIT_DIRECT, scale=INIT_DIRECT_SD) % 360)
+    
     estim_pos = np.array(INIT_POS, dtype=np.float16)
-
-    lost_ts_hist = np.empty(0, dtype=datetime)
+    lost_ts_buf = np.empty(0, dtype=datetime)
     t = BEGIN
     while t <= END:
-        print("main.py:", t.time())
+        print(f"main.py: {t.time()}")
         win = Window(t, log, map.resolution)
 
         for i in range(PARTICLE_NUM):
-            particles[i] = Particle(map.img, estim_pos, poses[i], directs[i])
+            particles[i] = Particle(map, poses[i], directs[i], estim_pos)
             particles[i].random_walk()
             particles[i].set_likelihood(estim_pos, map, win.strength_weight_list, win.subject_dist_list)
 
         poses, directs = resample(particles)
 
-        if LOST_TRAJECTORY_POLICY == 1:
+        if LOST_TJ_POLICY == 1:
             if not pf_param.IS_LOST:
                 estim_pos = pf_util.estim_pos(particles)
                 map.draw_particles(estim_pos, particles)
             if pf_param.TRUTH_LOG_FILE is not None:
-                map.draw_truth_pos(truth.update_err_hist(t, estim_pos, map.resolution, pf_param.IS_LOST), True)
+                map.draw_truth(truth.update_err(t, estim_pos, map.resolution, pf_param.IS_LOST), True)
 
-        elif LOST_TRAJECTORY_POLICY == 2:
+        elif LOST_TJ_POLICY == 2:
             if pf_param.TRUTH_LOG_FILE is not None and pf_param.IS_LOST:
                 last_estim_pos = estim_pos
-                lost_ts_hist = np.hstack((lost_ts_hist, t))
+                lost_ts_buf = np.hstack((lost_ts_buf, t))
             elif not pf_param.IS_LOST:
                 estim_pos = pf_util.estim_pos(particles)
                 map.draw_particles(estim_pos, particles)
 
                 if pf_param.TRUTH_LOG_FILE is not None:
-                    lerp_num = len(lost_ts_hist)
-                    for j, lt in enumerate(lost_ts_hist):
-                        map.draw_truth_pos(truth.update_err_hist(lt, pf_util.get_lerped_pos(estim_pos, last_estim_pos, j, lerp_num), map.resolution, True), True)
-                    lost_ts_hist = np.empty(0, dtype=datetime)
-                    map.draw_truth_pos(truth.update_err_hist(t, estim_pos, map.resolution, False), True)
-
-        map.show()
+                    buf_len = len(lost_ts_buf)
+                    for i, lt in enumerate(lost_ts_buf):
+                        map.draw_truth(truth.update_err(lt, pf_util.get_lerped_pos(estim_pos, last_estim_pos, i, buf_len), map.resolution, True), True)
+                    lost_ts_buf = np.empty(0, dtype=datetime)
+                    map.draw_truth(truth.update_err(t, estim_pos, map.resolution, False), True)
 
         if pf_param.ENABLE_SAVE_VIDEO:
             map.record()
+        if enable_show:
+            map.show()
 
         t += timedelta(seconds=pf_param.WIN_STRIDE)
 
@@ -100,7 +100,8 @@ def map_matching(conf: dict[str, Any]) -> None:
         pf_util.write_conf(conf, result_dir)
     if pf_param.TRUTH_LOG_FILE is not None:
         truth.export_err()
-    map.show(0)
+    if enable_show:
+        map.show(0)
 
 if __name__ == "__main__":
     import argparse
@@ -108,8 +109,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--conf_file", help="specify config file", metavar="PATH_TO_CONF_FILE")
+    parser.add_argument("--no_display", action="store_true", help="run without display")
+    args = parser.parse_args()
 
-    conf = set_params(parser.parse_args().conf_file)
+    conf = set_params(args.conf_file)
     _set_main_params(conf)
 
-    map_matching(conf)
+    map_matching(conf, not args.no_display)
