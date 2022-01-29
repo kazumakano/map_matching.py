bee089380ee2a02a2af17c461aa3fe79207b826f
kazumakano <kazumakano@outlook.jp>
2022-01-29 21:00:49 +0900

    apply no display

--- prepare_links.py
+++ prepare_links.py
@@ -6,7 +6,7 @@ from particle_filter.script.log import Log
 from script.map import Map
 
 
-def prepare_links(enable_csv: bool = False, enable_pkl: bool = False) -> None:
+def prepare_links(enable_show: bool = True, enable_csv: bool = False, enable_pkl: bool = False) -> None:
     map = Map(Log(datetime(2000, 1, 1), datetime(2000, 1, 1), glob(path.join(pf_param.ROOT_DIR, "log/observed/*.csv"))[0]).mac_list, None)    # whatever is fine
 
     if enable_csv:
@@ -15,7 +15,8 @@ def prepare_links(enable_csv: bool = False, enable_pkl: bool = False) -> None:
         map.export_links_to_pkl()
 
     map.draw_links()
-    map.show(0)
+    if enable_show:
+        map.show(0)
 
 if __name__ == "__main__":
     import argparse
@@ -25,6 +26,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--conf_file", help="specify config file", metavar="PATH_TO_CONF_FILE")
+    parser.add_argument("--no_display", action="store_true", help="run without display")
     parser.add_argument("--csv", action="store_true", help="export to CSV file")
     parser.add_argument("--pkl", action="store_true", help="export to pickle file")
     args = parser.parse_args()
@@ -36,4 +38,4 @@ if __name__ == "__main__":
     param.ENABLE_DRAW_LINKS = True
     param.SET_NODES_LINKS_POLICY = 1
 
-    prepare_links(args.csv, args.pkl)
+    prepare_links(not args.no_display, args.csv, args.pkl)
