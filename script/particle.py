59dd8c16e18649ec7c8208aaa202fbc6ee22d35a
kazumakano <kazumakano@outlook.jp>
2022-06-05 15:56:39 +0900

    update particle.py

--- script/particle.py
+++ script/particle.py
@@ -11,22 +11,23 @@ class Particle(PfParticle):
         super().set_likelihood(map.beacon_pos_list, strength_weight_list, subject_dist_list)
 
         if not pf_param.IS_LOST:    # don't consider match weight with old position
-            if param.MATCH_WEIGHT_POLICY == 1:      # multiply maximum match weight between nearest node and nodes linked with last node
-                min_cost = np.inf
-                near_node_index = map.get_nearest_node(self.pos)
-                for i in map.link_nodes[map.get_nearest_node(last_pos)]:
-                    cost = map.get_cost(i, near_node_index)
-                    if cost < min_cost:
-                        min_cost = cost
+            match param.MATCH_WEIGHT_POLICY:
+                case 1:    # multiply maximum match weight between nearest node and nodes linked with last node
+                    min_cost = np.inf
+                    near_node_idx = map.get_nearest_node(self.pos)
+                    for i in map.link_nodes[map.get_nearest_node(last_pos)]:
+                        cost = map.get_cost(i, near_node_idx)
+                        if cost < min_cost:
+                            min_cost = cost
 
-                self.likelihood *= pf_util.calc_prob_weight(min_cost, 0)
+                    self.likelihood *= pf_util.calc_prob_weight(min_cost, 0)
 
-            elif param.MATCH_WEIGHT_POLICY == 2:    # multiply match weight between nearest node and last node
-                self.likelihood *= pf_util.calc_prob_weight(map.get_cost(map.get_nearest_node(self.pos), map.get_nearest_node(last_pos)), 0)
+                case 2:    # multiply match weight between nearest node and last node
+                    self.likelihood *= pf_util.calc_prob_weight(map.get_cost(map.get_nearest_node(self.pos), map.get_nearest_node(last_pos)), 0)
 
-            elif param.MATCH_WEIGHT_POLICY == 3:    # pass or cut off
-                self.likelihood *= map.get_nearest_node(self.pos) in map.link_nodes[map.get_nearest_node(last_pos)]
+                case 3:    # pass or cut off
+                    self.likelihood *= map.get_nearest_node(self.pos) in map.link_nodes[map.get_nearest_node(last_pos)]
 
-            elif param.MATCH_WEIGHT_POLICY == 4:    # half reduction
-                if map.get_nearest_node(self.pos) not in map.link_nodes[map.get_nearest_node(last_pos)]:
-                    self.likelihood *= 0.5
+                case 4:    # half reduction
+                    if map.get_nearest_node(self.pos) not in map.link_nodes[map.get_nearest_node(last_pos)]:
+                        self.likelihood *= 0.5
